ace37f92e6ac1babee702876ff0308b110b9fb33
luthfifr <22187764+luthfifr@users.noreply.github.com>
2024-12-03 15:50:48 +0700

    modify ContentView.swift to trigger modal screen to show error message

--- SwiftUIToastAndSheet/ContentView.swift
+++ SwiftUIToastAndSheet/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.showError) private var showError
+    
     var body: some View {
         VStack {
             Button("Throw Error") {
-                
+                showError(error: SampleError.operationFailed, guidance: "Operation has failed. Please try again later.")
             }
         }
         .padding()
     }
 }
 
+// created to enable Preview works
+struct ContentViewContainer: View {
+    
+    @State private var errorWrapper: ErrorWrapper?
+    
+    var body: some View {
+        ContentView()
+            .environment(\.showError, ShowErrorAction(action: showError))
+            .sheet(item: $errorWrapper) { errorWrapper in
+                Text(errorWrapper.guidance
+                )
+            }
+    }
+    
+    private func showError(error: Error, guidance: String) {
+        errorWrapper = ErrorWrapper(error: error, guidance: guidance)
+    }
+}
+
 #Preview {
-    ContentView()
+    ContentViewContainer()
 }
