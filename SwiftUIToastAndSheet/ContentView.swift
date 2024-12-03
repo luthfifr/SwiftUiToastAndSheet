18cbbd25b97e12346e5ac6bddca9dfffce654374
luthfifr <22187764+luthfifr@users.noreply.github.com>
2024-12-03 13:37:53 +0700

    Initial Commit

---
+++ SwiftUIToastAndSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIToastAndSheet
+//
+//  Created by Luthfi Fathur Rahman on 03/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIToastAndSheet/SwiftUIToastAndSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToastAndSheetApp.swift
+//  SwiftUIToastAndSheet
+//
+//  Created by Luthfi Fathur Rahman on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToastAndSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
